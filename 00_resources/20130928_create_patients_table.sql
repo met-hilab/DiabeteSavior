7ae8a9a0a2f711f60e0761b5ba991e91cd3e5b16
WenjieShi <wenjie@wenjies-MacBook-Air.local>
2013-09-28 12:07:52 -0400

    New table for patients

---
+++ 00_resources/20130928_create_patients_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `cs673`.`patients` (
+  `id` INT NOT NULL,
+  `email` VARCHAR(255) NULL,
+  `firstname` VARCHAR(45) NULL,
+  `lastname` VARCHAR(45) NULL,
+  `dob` DATE NULL,
+  `gender` INT NULL,
+  `created` DATETIME NULL,
+  `modified` DATETIME NULL,
+  PRIMARY KEY (`id`));
