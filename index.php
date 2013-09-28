e04ea19fa6bcfbf62c3ba30511f31576d4392eec
CatCoache <yllewis@bu.edu>
2013-09-28 15:45:45 -0400

    ggg

--- index.php
+++ index.php
@@ -29,6 +29,8 @@
 //test here
 define('APP_DIR', 'app');
 define('DS', DIRECTORY_SEPARATOR);
+define('ROOT', dirname(__FILE__));
+define('WEBROOT_DIR', 'webrootasdsadass');
 
 define('WWW_ROOT', ROOT . DS . APP_DIR . DS . WEBROOT_DIR . DS);
 
