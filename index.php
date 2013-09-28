faecf7e2481740e574ce2731dd719045f2831d2c
Allen Yingyuan Zhang <zyy2013@bu.edu>
2013-09-28 15:42:40 -0400

    go

--- index.php
+++ index.php
@@ -27,8 +27,7 @@
  */
 define('APP_DIR', 'app');
 define('DS', DIRECTORY_SEPARATOR);
-define('ROOT', dirname(__FILE__));
-define('WEBROOT_DIR', 'webroot');
+
 define('WWW_ROOT', ROOT . DS . APP_DIR . DS . WEBROOT_DIR . DS);
 
 /**
