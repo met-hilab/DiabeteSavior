4cddd049d0205cdee6a9e7af1a958c2d759032d9
Jason Lu <jasonl.biz@gmail.com>
2013-10-16 16:24:40 -0400

    Required fields changed.

--- app/Model/Patient.php
+++ app/Model/Patient.php
@@ -84,7 +84,8 @@ class Patient extends AppModel {
 				//'last' => false, // Stop validation after this rule
 				//'on' => 'create', // Limit validation to 'create' or 'update' operations
 			),
-		),
+		)
+		/*
 		'gender' => array(
 			'notempty' => array(
 				'rule' => array('notempty'),
@@ -145,6 +146,7 @@ class Patient extends AppModel {
 				//'on' => 'create', // Limit validation to 'create' or 'update' operations
 			),
 		),
+		*/
 	);
 
 	//The Associations below have been created with all possible keys, those that are not needed can be removed
