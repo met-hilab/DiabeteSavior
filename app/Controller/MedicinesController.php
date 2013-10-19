c818959e6ff3213fcc631d529d84f36de9999c10
WenjieShi <wenjie@wenjies-MacBook-Air.local>
2013-10-19 18:15:07 -0400

    medicines controller

--- app/Controller/MedicinesController.php
+++ app/Controller/MedicinesController.php
@@ -30,7 +30,16 @@ App::uses('AppController', 'Controller');
  */
 class MedicinesController extends AppController {
 
+/**
+ * This controller does not use a model
+ *
+ * @var array
+ */
+	public $uses = array();
 
+public function add(){
+
+}
 public function View() {
 
 	}
