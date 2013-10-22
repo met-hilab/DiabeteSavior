839a9529319bf127ced613959e7ec853e11cb325
garimakumari <garima25@bu.edu>
2013-10-22 16:49:07 -0400

    Medicine controller

--- app/Controller/MedicinesController.php
+++ app/Controller/MedicinesController.php
@@ -1,27 +1,76 @@
 <?php
 /**
  * Created by JetBrains PhpStorm.
- * User: Singh/garima
+ * User: Singh
  * Date: 10/20/13
  * Time: 12:09 PM
  * To change this template use File | Settings | File Templates.
  */
+
 Class MedicinesController extends AppController {
 
-    public function index() {
+    public $uses = array();
+
+    /*public function index() {
 
         $Medicines= $this->Medicine->find('all');
 
         $this->set('Medicines',$Medicines);
     }
-
+*/
 
     public function add() {
-      if($this->request->is('Post'))
-      {
-         $this->Medicine->save($this->request->data);
+      /*if($this->request->is('Post'))
+      $this->Medicine->save($this->request->data);
       }
-    }
+*/
+       $this->Session->delete('medicine_id');
+
+        if ($this->request->is('post')){
+            $medicine = $this->request->data['medicine'];
+            $medicine_name = $medicine['medicine_name'];
+            $min_dose = $medicine['min_dose'];
+            $max_dose = $medicine['max_dose'];
+            $matric = $medicine['matric'];
+            $hypo = $medicine['hypo'];
+            $weight = $medicine['weight'];
+            $renal_gu = $medicine['renal_gu'];
+            $gi_sx = $medicine['gi_sx'];
+            $chf = $medicine['chf'];
+            $cvd = $medicine['cvd'];
+            $bone= $medicine['bone'];
+
+            $data = $medicine;
+            $this->Medicine->create();
+            //$res = $this->Patient->save($this->data);
+
+            if($this->Medicine->save($this->data)){
+
+                //$this->Session->setFlash($_SESSION["patientnum"].' Patient is saved.');
+                //$patient_number = $_SESSION["patientnum"];
+                //$conditions = array("patient_number" => $patient_number);
+                //$conditions = array("patient_number" => "M0000001");
+                //$patient = $this->Patient->find('first', array('conditions' => $conditions));
+                //$patient = $patient['Patient'];
+                $id = $this->Medicine->getLastInsertId();
+                $Medicine = $this->Medicine->findById($id);
+                $this->Session->write('Medicine_id', $id);
+                $this->Session->write('Medicine', $Medicine);
+                //var_dump($_SESSION); exit;
+                //$this->set('patient', $patient);
+                //$_SERVER['patient_number'] = $patient_number;
+                //$this->redirect(array('action'=>'view',$patient_number));
+                $this->redirect(array('action'=>'show'));
+
+            }else{
+                $this->Session->setFlash('medicine is not saved.');
+                //$this->redirect(array('action'=>'add'));
+            }
+        }
+
+}
+
+
 
     public function edit(){
 
