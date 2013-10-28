08522c98d5d01bf5846729c332f01121e212bc58
jandrebu <jandre@bu.edu>
2013-10-28 18:20:06 -0400

    Patient model update for postal code allow empty

--- app/Model/Patient.php
+++ app/Model/Patient.php
@@ -122,7 +122,7 @@ class Patient extends AppModel {
 			'postal' => array(
 				'rule' => array('postal'),
 				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
+				'allowEmpty' => true,
 				//'required' => false,
 				//'last' => false, // Stop validation after this rule
 				//'on' => 'create', // Limit validation to 'create' or 'update' operations
