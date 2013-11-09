7b1541aa84038e829b6fab19dfdc05a56b0aa37d
xueyike(Eva) <xueyike@gmail.com>
2013-11-09 15:23:10 -0800

    update DrugAllegy model

--- app/Model/DrugAllergy.php
+++ app/Model/DrugAllergy.php
@@ -24,7 +24,27 @@ class DrugAllergy extends AppModel {
 		'su_gln' => 'notempty',
 		'insulin' => 'notempty',
 		'sglt_2' => 'notempty',
-		'praml' => 'notempty'
+		'praml' => 'notempty' ,
+		'created' => array(
+			'datetime' => array(
+				'rule' => array('datetime'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'modified' => array(
+			'datetime' => array(
+				'rule' => array('datetime'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
 	);
 
 	//The Associations below have been created with all possible keys, those that are not needed can be removed
